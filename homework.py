9524d03179adaca9179eabd83f33992edc66c538
Kateryna Cherepanova <absent.kai@gmail.com>
2022-04-16 23:56:18 +0300

    pytest

--- homework.py
+++ homework.py
@@ -13,12 +13,12 @@ class InfoMessage:
         self.speed = speed
         self.calories = calories
 
-    def get_message(self) -> None:
-        print(f'Тип тренировки: {self.training_type}; '
-              f'Длительность: {self.duration: .3f} ч.; '
-              f'Дистанция: {self.distance: .3f} км; '
-              f'Ср. скорость: {self.speed: .3f} км/ч; '
-              f'Потрачено ккал: {self.calories: .3f}. ')
+    def get_message(self) -> str:
+        return (f'Тип тренировки: {self.training_type}; '
+                f'Длительность: {self.duration:.3f} ч.; '
+                f'Дистанция: {self.distance:.3f} км; '
+                f'Ср. скорость: {self.speed:.3f} км/ч; '
+                f'Потрачено ккал: {self.calories:-.3f}.')
 
 
 class Training:
@@ -27,6 +27,8 @@ class Training:
     LEN_STEP: float = 0.65
     # константа для перевода значений из метров в километры
     M_IN_KM: int = 1000
+    # колличество минут в часе
+    M = 60
 
     def __init__(self,
                  action: int,
@@ -45,7 +47,7 @@ class Training:
 
     def get_mean_speed(self) -> float:
         """Получить среднюю скорость движения."""
-        return self.get_distance() * self.duration
+        return self.get_distance() / self.duration
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
@@ -69,7 +71,7 @@ class Running(Training):
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
         return ((self.COEFF_1 * self.get_mean_speed() - self.COEFF_2)
-                * self.weight / self.M_IN_KM * self.duration)
+                * self.weight / self.M_IN_KM * self.duration * self.M)
 
 
 class SportsWalking(Training):
@@ -91,7 +93,7 @@ class SportsWalking(Training):
         """Получить количество затраченных калорий."""
         return ((self.COEFF_1 * self.weight
                  + (self.get_mean_speed() ** self.COEFF_2 // self.height)
-                 * self.COEFF_3 * self.weight) * self.duration)
+                 * self.COEFF_3 * self.weight) * self.duration * self.M)
 
 
 class Swimming(Training):
