35dddd502ef99357de9ff13976f2e3867fef6e78
Kateryna Cherepanova <absent.kai@gmail.com>
2022-04-18 20:25:23 +0300

    rev1_1

--- homework.py
+++ homework.py
@@ -1,8 +1,12 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 from typing import Type
 
+SWM: str = 'SWM'
+RUN: str = 'RUN'
+WLK: str = 'WLK'
 
-@dataclass
+
+@dataclass(frozen=True)
 class InfoMessage:
     """Информационное сообщение о тренировке."""
     training_type: str
@@ -10,20 +14,21 @@ class InfoMessage:
     distance: float
     speed: float
     calories: float
+    TEXT: str = ('Тип тренировки: {training_type}; '
+                 'Длительность: {duration:.3f} ч.; '
+                 'Дистанция: {distance:.3f} км; '
+                 'Ср. скорость: {speed:.3f} км/ч; '
+                 'Потрачено ккал: {calories:.3f}.')
 
     def get_message(self) -> str:
-        return (f'Тип тренировки: {self.training_type}; '
-                f'Длительность: {self.duration:.3f} ч.; '
-                f'Дистанция: {self.distance:.3f} км; '
-                f'Ср. скорость: {self.speed:.3f} км/ч; '
-                f'Потрачено ккал: {self.calories:.3f}.')
+        return self.TEXT.format(**asdict(self))
 
 
 class Training:
     """Базовый класс тренировки."""
     LEN_STEP: float = 0.65
     M_IN_KM: int = 1000
-    MINUTES_IN_HOUR = 60
+    MINUTES_IN_HOUR: int = 60
 
     def __init__(self,
                  action: int,
@@ -105,7 +110,6 @@ class Swimming(Training):
                  ) -> None:
         super().__init__(action, duration, weight)
         self.length_pool = length_pool
-        # сколько раз пользователь переплыл бассейн
         self.count_pool = count_pool
 
     def get_mean_speed(self) -> float:
@@ -126,7 +130,7 @@ def read_package(workout_type: str, data: list[int]) -> Training:
                                                 'WLK': SportsWalking}
     if workout_type in workout_types:
         return workout_types[workout_type](*data)
-    raise ValueError
+    raise ValueError('Тип тренировки не предусмотрен')
 
 
 def main(training: Training) -> None:
@@ -137,9 +141,9 @@ def main(training: Training) -> None:
 
 if __name__ == '__main__':
     packages = [
-        ('SWM', [720, 1, 80, 25, 40]),
-        ('RUN', [15000, 1, 75]),
-        ('WLK', [9000, 1, 75, 180])
+        (SWM, [720, 1, 80, 25, 40]),
+        (RUN, [15000, 1, 75]),
+        (WLK, [9000, 1, 75, 180])
     ]
 
     for workout_type, data in packages:
